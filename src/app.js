2a698f61c3c2d55d1ed803348de70a486f5cdaba
CarolineG85 <caroline.gallix85@gmail.com>
2023-11-27 20:39:11 +0100

    get api users created

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
+const port = process.env.APP_PORT;
 
 const movieControllers = require("./controllers/movieControllers");
+//import de la database à ne pas oublier
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
+app.get("/api/users", movieControllers.getUsers);
+app.get("/api/users/:id", movieControllers.getUsersById);
+
+
+
 
 module.exports = app;
