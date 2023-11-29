33a4093e688c1935fbcfed285ba09c80e6e70762
CarolineG85 <caroline.gallix85@gmail.com>
2023-11-29 11:09:38 +0100

    creating delete route for users

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -175,6 +175,46 @@ const updateUser = (req, res) => {
     });
 };
 
+
+const deleteMovie = (req, res) => {
+const id = parseInt(req.params.id);
+
+
+database.query("delete from movies where id = ?", [id])
+.then(([result]) => {
+  if (result.affectedRows === 0) {
+    res.sendStatus(404);
+  } else {
+    res.sendStatus(204);
+  }
+})
+.catch((err) => {
+  console.error(err);
+  res.sendStatus(500);
+});
+
+}
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  
+  
+  database.query("delete from users where id = ?", [id])
+  .then(([result]) => {
+    if (result.affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.sendStatus(500);
+  });
+  
+  
+  }
+
 module.exports = {
   getMovies,
   getMovieById,
@@ -185,5 +225,8 @@ module.exports = {
   postUser,
 
   updateMovie,
-  updateUser
+  updateUser,
+
+  deleteMovie,
+  deleteUser
 };
